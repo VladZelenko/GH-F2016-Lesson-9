0682845ba7a9f86fb35dc88849af725b1fcb70a0
Vlad6555 <Vlad6555@ukr.net>
2016-12-15 20:02:43 +0200

    bootstrap start page design

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 //плашины которые подключаем в gulp
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
-		browserSync = require('browser-sync');
+		browserSync = require('browser-sync'),
+		concat 			= require('gulp-concat'),
+		uglify			= require('gulp-uglifyjs');
 
 //компилятор сас
 gulp.task("sass", function() {
-	return gulp.src('app/sass/main.scss')
+	return gulp.src('app/sass/**/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('app/css'))
 		.pipe(browserSync.reload({stream: true}))
 });
+
+//сжатие и обьединение js скриптов в один
+//gulp.task('scripts', function() {
+//    return gulp.src([ // Берем все необходимые библиотеки
+ //       'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
+//        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+//        ])
+ //       .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+//        .pipe(gulp.dest('app/js')); // Выгружаем в папку app/js
+//});
 
 //live reload in gulp
 gulp.task('browser-sync', function() {
@@ -22,8 +34,8 @@ gulp.task('browser-sync', function() {
 });
 
 //смотрит за изменениями в сас файлах и выполняет компиляцию, а также подлючает другие таски
-gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch('app/sass/main.scss', ['sass']);
+gulp.task('watch', ['browser-sync', 'sass'], function() {//добавить 'scripts'
+    gulp.watch('app/sass/**/*.scss', ['sass']);
     gulp.watch('app/**/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
